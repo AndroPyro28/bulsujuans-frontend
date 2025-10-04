f051ffd11af580479b188eb48f25940f154fa968
AndroPyro28 <menandroeugenio1028@gmail.com>
2025-10-04 17:09:19 +0800

    feat: add image compression

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,24 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
-
+import imageCompression from "browser-image-compression";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const handleImageCompression = async (file: File) => {
+  const options = {
+    maxSizeMB: 1,
+    maxWidthOrHeight: 1920,
+    useWebWorker: true,
+  };
+  try {
+    // console.log('before compressed', file.size / 1024 / 1024 + 'MB')
+    const compressedFile = await imageCompression(file, options);
+    // console.log(`compressedFile size ${compressedFile.size / 1024 / 1024} MB`); // smaller than maxSizeMB
+
+    // console.log('compressed file', compressedFile)
+    return compressedFile;
+  } catch (error) {
+    console.log(error);
+  }
+};

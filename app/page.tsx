464b25792ff78daedf3ce40fbce43514c7154067
AndroPyro28 <menandroeugenio1028@gmail.com>
2025-10-04 23:41:47 +0800

    feat: socket introductionb

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,23 @@
+"use client"
+import { io } from "socket.io-client";
 import { ModeToggle } from "@/components/dark-mode";
 import Image from "next/image";
+import { useEffect } from "react";
 
 export default function Home() {
+
+  const socket = io("http://localhost:3001");
+
+  useEffect(() => {
+    socket.on("hey", (value) => {
+        console.log(`hey ${value}`)
+    });
+
+    return () => {
+      socket.off("receive_message");
+    };
+  }, [])
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
